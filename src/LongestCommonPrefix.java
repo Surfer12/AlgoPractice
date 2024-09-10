913229e3050913a8a616a6921e3a8c2ca95d3416
Surfer12 <ryanoatsie@outlook.com>
2024-09-10 02:45:50 -0700

    chore: Refactor LongestCommonPrefix class to use Optional for null check

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -18,14 +18,14 @@ public class LongestCommonPrefix {
             return Optional.empty();
         }
         String prefix = strs[0];
-        for (String str : strs) {
-            while (!str.startsWith(prefix)) {
+        for(String str : strs) {
+            while(!str.startsWith(prefix)) {
                 prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
+                if(prefix.isEmpty()) {
                     return Optional.empty();
                 }
             }
         }
         return Optional.of(prefix);
-    }
+    }  
 }
